a385d82b78e5732485e3749a914a3cfdfc2e5a1a
Raquel Freitas <100779408+rakeu@users.noreply.github.com>
2024-10-22 17:17:04 -0300

    Update .js

--- .js
+++ .js
@@ -3,27 +3,17 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result"); 
 //metodo utilizado para obter o elemento de entrada id armazenando o mesmo em uma variavel 
 
-function decimalToBinary (input) {
-    const inputs = [];
-    const quotients = []; 
-    const remainders = [];
+const decimalToBinary = (input) => {
+if (input === 0 || input === 1) {
+    return (String(input));
+} else {
+   return decimalToBinary(Math.floor(input/2)) + (input % 2); 
+}
+}
 
-    
-    while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
-    inputs.push(input); //anexar o input ao array inputs
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient; 
-    };
-console.log("inputs: ", inputs);
-console.log("Quotients: ", quotients);
-console.log("Remainders: ", remainders);
-remainders.reverse().join("");
-result.innerText = "";   
-};
-
+const showAnimation = () => {
+if (parseInt(numberInput.value === 5)) {}
+}
 
 const checkUserInput = () => {
 if (!numberInput.value || //entrada numerica: verifica se o valor da entrada numerica for vazia 
@@ -34,6 +24,7 @@ alert("Please provide a decimal number greater than or equal to 0");
 return   
 } 
 decimalToBinary(parseInt(numberInput.value)); 
+result.textContent = decimalToBinary()
 numberInput.value = "" 
 }; 
 
